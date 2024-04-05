0b049c64e10c12a209d5c5323a8a03e332b21fcf
dinadendan <dina.dendan222@gmail.com>
2024-04-05 05:50:22 +0200

    add EquipmentTab

--- lib/features/my_trainer/ui/widgets/equipment_tab.dart
+++ lib/features/my_trainer/ui/widgets/equipment_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:modarb_app/core/helper/spacing.dart';
 import 'package:modarb_app/core/theming/colors.dart';
 import 'package:modarb_app/core/theming/styles.dart';
 
@@ -16,15 +17,23 @@ class EquipmentTab extends StatelessWidget{
           padding: EdgeInsets.symmetric (horizontal: 10.w, vertical: 15.h),
           child: Container(
             width: double.infinity,
+            height: 100.h,
             decoration: const BoxDecoration(
               color: ColorsManager.lighterGray,
               borderRadius: BorderRadius.all(Radius.circular(16.0),),
             ),
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Row(
                 children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(16.r),
+                    child: Image.asset(
+                        'assets/images/Muscles1.png',
+                      scale: 1,
+                      ),
+                  ),
+                  horizontalSpace(20),
                   Text(
                     'Muscles Worked',
                     style: TextStyles.font16White700,
