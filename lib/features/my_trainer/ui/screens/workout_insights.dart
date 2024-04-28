d11863a548a32c199179d6090b42a24644b98f2b
dinadendan <dina.dendan222@gmail.com>
2024-04-29 00:48:18 +0200

    some edit in WorkoutInsights

--- lib/features/my_trainer/ui/screens/workout_insights.dart
+++ lib/features/my_trainer/ui/screens/workout_insights.dart
@@ -12,80 +12,84 @@ class WorkoutInsights extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          constraints:  const BoxConstraints.expand(),
-          foregroundDecoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment(0.1, .8),
-              end: Alignment(0.01, -1),
-              colors: [Color(0xFF5E35E8), Color(0x00D9D9D9)],
+        child: Stack(
+          children:[
+            Container(
+              constraints:  const BoxConstraints.expand(),
+              foregroundDecoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment(0.1, .8),
+                  end: Alignment(0.01, -1),
+                  colors: [Color(0xFF5E35E8), Color(0x00D9D9D9)],
+                ),
+              ),
             ),
-          ),
-          child: Padding(
-            padding:  EdgeInsets.symmetric(horizontal: 15.w,vertical: 60.h),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Workout\n insights',
-                style: TextStyles.font28White700,
-                ),
-                verticalSpace(30),
-                Text(
-                  'Day 1',
-                style: TextStyles.font19White700,
-                ),
-                Text(
-                  'full body ',
-                  style: TextStyles.font19White700,
-                ),
-                verticalSpace(30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '       6\nExercises',
-                      style: TextStyles.font16White700,
-                    ),
-                    Text(
-                      ' 20\nsets',
-                      style: TextStyles.font16White700,
-                    ),
-                    Text(
-                      '  30:00\nDuration',
-                      style: TextStyles.font16White700,
-                    ),
-                  ],
-                ),
-                verticalSpace(40),
-                Text(
-                  'Post Workout meal ',
-                  style: TextStyles.font19White700,
-                ),
-                verticalSpace(20),
-                Expanded(
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (BuildContext context, int index)=> Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset('assets/images/nutrition.png'),
-                    ),
-                    itemCount: 2,
-
+            Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 15.w,vertical: 60.h),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Workout\n insights',
+                    style: TextStyles.font28White700,
                   ),
-                ),
-                verticalSpace(20),
-                AppTextButton(
-                    buttonText: 'Finish',
-                    // textStyle: TextStyles.font20Purple600,
-                    textColor: ColorsManager.mainPurple,
-                    backgroundColor: Colors.white.withOpacity(0.9),
-                    onPressed: (){}
-                ),
-                
-              ],
+                  verticalSpace(30),
+                  Text(
+                    'Day 1',
+                    style: TextStyles.font19White700,
+                  ),
+                  Text(
+                    'full body ',
+                    style: TextStyles.font19White700,
+                  ),
+                  verticalSpace(30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '       6\nExercises',
+                        style: TextStyles.font16White700,
+                      ),
+                      Text(
+                        ' 20\nsets',
+                        style: TextStyles.font16White700,
+                      ),
+                      Text(
+                        '  30:00\nDuration',
+                        style: TextStyles.font16White700,
+                      ),
+                    ],
+                  ),
+                  verticalSpace(40),
+                  Text(
+                    'Post Workout meal ',
+                    style: TextStyles.font19White700,
+                  ),
+                  verticalSpace(20),
+                  Expanded(
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index)=> Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset('assets/images/nutrition.png'),
+                      ),
+                      itemCount: 2,
+
+                    ),
+                  ),
+                  verticalSpace(20),
+                  AppTextButton(
+                      buttonText: 'Finish',
+                      // textStyle: TextStyles.font20Purple600,
+                      textColor: ColorsManager.mainPurple,
+                      backgroundColor: Colors.white.withOpacity(0.9),
+                      onPressed: (){}
+                  ),
+
+                ],
+              ),
             ),
-          ),
+          ]
         ),
       ),
     );
